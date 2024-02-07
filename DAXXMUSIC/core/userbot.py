8705c379c7a4299c341e4c4e8769af0c71df2594
STRANGEROP <139733000+Shivasengar12@users.noreply.github.com>
2024-02-07 08:47:04 +0530

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("dreamyworldxD")
+                await self.one.join_chat("EquinoxNetwork")
               
-                await self.one.join_chat("strangers_bots")
+                await self.one.join_chat("Equinox_chats")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("dreamyworldxD")
+                await self.two.join_chat("EquinoxNetwork")
                 
-                await self.one.join_chat("strangers_bots")
+                await self.one.join_chat("Equinox_chats")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("dreamyworldxD")
+                await self.three.join_chat("EquinoxNetwork")
                 
-                await self.one.join_chat("strangers_bots")
+                await self.one.join_chat("Equinox_chats")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("dreamyworldxD")
+                await self.four.join_chat("EquinoxNetwork")
                 
-                await self.one.join_chat("strangers_bots")
+                await self.one.join_chat("Equinox_chats")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("dreamyworldxD")
+                await self.five.join_chat("EquinoxNetwork")
                 
-                await self.one.join_chat("strangers_bots")
+                await self.one.join_chat("Equinox_chats")
             except:
                 pass
             assistants.append(5)

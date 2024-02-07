80510a108d19cc94f2864ad60a12240309e96f71
STRANGEROP <139733000+Shivasengar12@users.noreply.github.com>
2024-02-07 08:34:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME", "NoMoreTyMWaStE")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "EquinoxNetwork")
 # Get Your bot username
 BOT_USERNAME = getenv("BOT_USERNAME", "alexaprobot")
 # Don't Add style font 
@@ -51,8 +51,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/strangers_bots")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/dreamyworldxD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Equinoxnetwork")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EquinoxSupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
